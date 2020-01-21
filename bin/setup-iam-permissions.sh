e850d1e1751a691bd42ab9103cd3790152d9e368
Rob Morgan <robbym@gmail.com>
2020-01-21 10:28:28 +0800

    add more apis

--- bin/setup-iam-permissions.sh
+++ bin/setup-iam-permissions.sh
@@ -7,6 +7,18 @@ if [[ -z "${GOOGLE_CLOUD_PROJECT}" ]]; then
   exit 1
 fi
 
+echo "Enabling the Cloud Resource Manager API for project $GOOGLE_CLOUD_PROJECT..."
+gcloud services enable cloudresourcemanager.googleapis.com —project=$GOOGLE_CLOUD_PROJECT
+
+echo "Enabling the Cloud Build API for project $GOOGLE_CLOUD_PROJECT..."
+gcloud services enable cloudbuild.googleapis.com —project=$GOOGLE_CLOUD_PROJECT
+
+echo "Enabling the Container Registry API for project $GOOGLE_CLOUD_PROJECT..."
+gcloud services enable containerregistry.googleapis.com —project=$GOOGLE_CLOUD_PROJECT
+
+echo "Enabling the Cloud Run API for project $GOOGLE_CLOUD_PROJECT..."
+gcloud services enable run.googleapis.com —project=$GOOGLE_CLOUD_PROJECT
+
 echo "Discovering Project ID for project $GOOGLE_CLOUD_PROJECT..."
 PROJECT_NUM=$(gcloud projects describe $GOOGLE_CLOUD_PROJECT --format='value(projectNumber)')
 
